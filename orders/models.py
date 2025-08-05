3b63edc97a3aa94615653836233c3f38eb39e587
dimayasinskyi <dmitriymorkov83@gmail.com>
2025-08-05 14:11:11 +0300

    19299

--- orders/models.py
+++ orders/models.py
@@ -1,9 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-from products.models import Item
+
+class OrderStatusChoices(models.TextChoices):
+    """
+    Order status selection class for the Order model.
+
+    Options to choose from:
+    - Pending
+    - Processing
+    - Shipped
+    - Delivered
+    - Cancelled
+    - Refunded
+    """
+    PENDING = "pending", "Pending"
+    PROCESSING = "processing", "Processing"
+    SHIPPED = "shipped", "Shipped"
+    DELIVERED = "delivered", "Delivered"
+    CANCELLED = "cancelled", "Cancelled"
+    REFUNDED = "pefunded", "Pefunded"
 
 
 class Order(models.Model):
-    customer
-    items
-    status
+    """
+    Has fields:
+    - customer: will be associated with the User model type many-to-one cascade delete
+    - oreder_items: will be associated with the Item model many-to-many relationship (related name "items")
+    - total_amout: should contain the total amount that fills the signal (optional) 
+    - status: dropdown list based on class OrderStatusChoises (defaut is processing)
+
+    Method:
+    - __str__: returns full customer name and total amount (f"{ self.customer.get_full_name() } | { self.total_amount }")
+    """
+    customer = models.ForeignKey(to=User, on_delete=models.CASCADE, verbose_name="Customer")
+    order_items = models.ManyToManyField(to="products.Item", related_name="items", verbose_name="Items")
+    total_amount = models.DecamilField(max_digits=10, desimal_places=2, blank=True, null=True, verbose_name="Total amount")
+    status = models.CharField(
+        max_length=50, 
+        choices=OrderStatusChoices.choices, 
+        default=OrderStatusChoices.PROCESSING, 
+        verbose_name="Status"
+        )
+
+    class Meta:
+        ordering = ["status"]
+        verbose_name = "Order"
+        verbose_name_plural = "Orders"
+    
+    def __str__(self):
+        return f"{ self.customer.get_full_name() } | { self.total_amount }"
