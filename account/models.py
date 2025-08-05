f722e395ccfbc6231945323dd86f093e82ac9df9
dimayasinskyi <dmitriymorkov83@gmail.com>
2025-08-05 12:02:56 +0300

    19297

--- account/models.py
+++ account/models.py
@@ -1,1 +1,6 @@
-from django.db import models
+from django.db import models 
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.)
