bd5dcdf899605bd670e7edfa56b61bc275559933
dimayasinskyi <dmitriymorkov83@gmail.com>
2025-08-06 22:33:35 +0300

    19298

--- products/urls.py
+++ products/urls.py
@@ -8,6 +8,6 @@ app_name = "product"
 urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list'),
     path('items/<int:id>/', ItemDetailView.as_view(), name='item-detail'),
-    path('menu/', MenuItemView.as_view(), name='menu-list'),
+
     path('menu/<int:id>/', MenuItemDetailView.as_view(), name='menu-detail'),
 ]
