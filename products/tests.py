d8900720d2d28a78b14f0411cc3fa31b014edc54
dimayasinskyi <dmitriymorkov83@gmail.com>
2025-08-06 19:14:41 +0300

    19301

--- products/tests.py
+++ products/tests.py
@@ -1,23 +1,45 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase
-from rest_framework import status
 
 from .models import Item
+from .serializers import ItemSerializer
 from .views import ItemDetailView
 
 
 class ItemDetailViewTests(APITestCase):
     def setUp(self):
-        self.valid_data = {
+        valid_data = {
             "item_name": "test name",
             "item_price": 12.99,
         }
-        self.url = reverse("produtc:item-detail")
+        item = Item.objects.create(**valid_data)
+        self.serializers = ItemSerializer(item)
+        self.url = reverse("produtc:item-detail", kwargs=item.id)
 
     def test_get_method(self):
-        item = Item.objects.create(**self.valid_data)
         response = self.client.get(self.url)
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.json(), )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), self.serializers.data)
 
+    def test_put_method(self):
+        put_data = {
+            "item_name": "test name2",
+            "item_price": 10,
+        }
+        response = self.client.put(self.url, data=put_data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response['item_name'], put_data['item_name'])
+        self.assertEqual(response['item_price'], put_data['item_price'])
+
+    def test_put_method(self):
+        patch_data = {
+            "item_name": "test name2",
+        }
+        response = self.client.patch(self.url, data=put_data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response['item_name'], put_data['item_name'])
+
+    
