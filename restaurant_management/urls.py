b21e0969eed58f579e0adad9206561e96927db57
dimayasinskyi <dmitriymorkov83@gmail.com>
2025-08-06 22:30:37 +0300

    19298

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('home.urls')),
