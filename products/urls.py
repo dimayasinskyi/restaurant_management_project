af14af2f4a806a81d310e54b2bfe0608b7fffda8
dimayasinskyi <dmitriymorkov83@gmail.com>
2025-08-06 18:36:45 +0300

    19301

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from .views import *
 
 
+app_name = "product"
+
 urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list'),
     path('items/<int:id>/', ItemDetailView.as_view(), name='item-detail'),
