dd3bc940ed4d1163b64ae8fbcb77b5c2b4ea0edc
dimayasinskyi <dmitriymorkov83@gmail.com>
2025-08-06 18:37:52 +0300

    19301

--- products/tests.py
+++ products/tests.py
@@ -2,9 +2,15 @@ from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
 
+from .models import Item
 from .views import ItemDetailView
 
 
 class ItemDetailViewTests(APITestCase):
     def setUp(self):
+        self.valid_data = {
+            
+        }
         self.url = reverse("produtc:item-detail")
+
+
