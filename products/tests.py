5fb8a8b9d440ee727384924cd92944554f0f8876
dimayasinskyi <dmitriymorkov83@gmail.com>
2025-08-06 18:38:53 +0300

    19301

--- products/tests.py
+++ products/tests.py
@@ -9,7 +9,8 @@ from .views import ItemDetailView
 class ItemDetailViewTests(APITestCase):
     def setUp(self):
         self.valid_data = {
-            
+            "item_name": "test name",
+            "item_price": 
         }
         self.url = reverse("produtc:item-detail")
 
