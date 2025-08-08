5e0ebb9637f5d3b77123ccb8f258578d1b8a0b26
dimayasinskyi <dmitriymorkov83@gmail.com>
2025-08-08 23:56:00 +0300

    24598

--- home/views.py
+++ home/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.conf import settings
 
 
+app_name = "home"
+
 def about(request):
     return render(request, "about.html", {"restaurant": settings.RESTAURANT_NAME})
