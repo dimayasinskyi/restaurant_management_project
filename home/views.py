517184b6da383628d0b7ee4dcc84c57121a5652c
dimayasinskyi <dmitriymorkov83@gmail.com>
2025-08-09 22:37:09 +0300

    28382

--- home/views.py
+++ home/views.py
@@ -4,5 +4,8 @@ from django.conf import settings
 
 app_name = "home"
 
+def main_page(request):
+    return render(request, "home.html", {"restaurant_name": settings.RESTAURANT_NAME})
+
 def about(request):
     return render(request, "about.html", {"restaurant_name": settings.RESTAURANT_NAME})
