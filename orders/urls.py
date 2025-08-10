10e5d618501b2cc7e1cc7cb9d71e2946cdbeb4b6
dimayasinskyi <dmitriymorkov83@gmail.com>
2025-08-11 02:23:21 +0300

    29422

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import *
 
+
+app_name = "order"
+
 urlpatterns = [
-    path('reservation/', reservation)
+    path("reservation/", reservation, name="reservation")
 ]
