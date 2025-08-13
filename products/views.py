a3acd2b2e0a269604ec3f2f848a78173d73f4d91
dimayasinskyi <dmitriymorkov83@gmail.com>
2025-08-13 15:20:33 +0300

    29425

--- products/views.py
+++ products/views.py
@@ -2,12 +2,11 @@ from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.http import Http404
-from djngo.db import DatebaseError
+from django.db import DatebaseError
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from datetime import datetime
-
 
 from .models import Item
 from .serializers import ItemSerializer
