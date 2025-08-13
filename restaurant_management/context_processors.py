295ecfdcdda65d5b368ead8754465f02b53d099f
dimayasinskyi <dmitriymorkov83@gmail.com>
2025-08-13 16:03:09 +0300

    32153

--- restaurant_management/context_processors.py
+++ restaurant_management/context_processors.py
@@ -5,4 +5,5 @@ def restaurant_info(request):
     return {
         "restaurant_name": settings.RESTAURANT_NAME,
         "restaurant_phone": settings.RESTAURANT_CONTACT_PHONE,
+        "restaurant_working_hours": settings.WORKING_HOURS,
     }
