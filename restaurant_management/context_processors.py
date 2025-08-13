11e815e4a298f47f242e431d8ed9fbdafd1b6351
dimayasinskyi <dmitriymorkov83@gmail.com>
2025-08-13 15:39:22 +0300

    32151

--- restaurant_management/context_processors.py
+++ restaurant_management/context_processors.py
@@ -1,7 +1,7 @@
 from django.conf import setiings
 
 
-def restaaurant_info(request):
+def restaurant_info(request):
     return {
         "restaurant_name": setiings.RESTAURANT_NAME,
         "restaurant_phone": setiings.RESTAURANT_CONTACT_PHONE,
