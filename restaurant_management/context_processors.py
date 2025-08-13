ef7e25c6f49e706c22ace421e01b596d4fd6d944
dimayasinskyi <dmitriymorkov83@gmail.com>
2025-08-13 16:17:53 +0300

    32153

--- restaurant_management/context_processors.py
+++ restaurant_management/context_processors.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 def restaurant_info(request):
     return {
-        "restaurant_name": settings.RESTAURANT_NAME,
-        "restaurant_phone": settings.RESTAURANT_CONTACT_PHONE,
-        "restaurant_working_hours": settings.WORKING_HOURS,
+        "restaurant_name": getattr(settings, "RESTAURANT_NAME", "Restaurant name not provided."),
+        "restaurant_phone": getattr(settings, "RESTAURANT_CONTACT_PHONE", "Contact phone number not provided."),
+        "restaurant_working_hours": getattr(settings, "WORKING_HOURS", [{"days": "Working hours not provided", "hours": ""}]),
     }
