8ed168f6db4d72a3d8f74cab3906ac39065b80c2
dimayasinskyi <dmitriymorkov83@gmail.com>
2025-08-15 20:14:58 +0300

    32152

--- account/form.py
+++ account/form.py
@@ -1,1 +1,14 @@
+from django import forms
+
 from .models import Feetback
+
+
+class Feetback(forms.ModelForm):
+    """
+    Form for leaving comments on menu items.
+
+    Has only a text field.
+    """
+    def Meta:
+        model = Feetback
+        fields = ["text"]
