0bd46f4846d6c5fed40b27c618e5588c4958121d
dimayasinskyi <dmitriymorkov83@gmail.com>
2025-08-16 04:32:28 +0300

    33866

--- home/models.py
+++ home/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class RestaurantLocationModel(models.Model):
+    """
+    Model for storing information about the location of a restaurant.
+
+    Has fields:
+    - address: text field
+    - city: field characters maximum 189
+    - state: field characters maximum 14 (optionally)
+    - zip_code: field characters maximum 10
+    - created_at: automatically filled with the current time
+    """
+    address = models.TextField(verbose_name="Address")
+    city = models.CharField(max_length=189, verbose_name="City")
+    state = models.CharField(max_length=14, blank=True, verbose_name="State")
+    zip_code = models.CharField(max_length=10, verbose_name="Zip")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created at")
+
+    class Meta:
+        ordering = ["city"]
+        verbose_name = "Restaurant Location"
+        verbose_name_plural = "Restaurants Location"
+
+    def __str__(self):
+        return self.city
