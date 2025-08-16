b618db2291cb8f020c5c3bc03eeaf8746aa3180c
dimayasinskyi <dmitriymorkov83@gmail.com>
2025-08-16 05:32:17 +0300

    33862

--- home/views.py
+++ home/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
 from django.conf import settings
 
-from .models import RestaurantLocationModel
-
 
 app_name = "home"
 
 def main_page(request):
-    return render(request, "home.html" {"restaurant_location": RestaurantLocationModel.objects.all()})
+    return render(request, "home.html")
 
 def about(request):
     return render(request, "about.html")
