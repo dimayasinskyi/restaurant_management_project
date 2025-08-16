b0efe6b897f8b541f03a0c14d264870da8062fd5
dimayasinskyi <dmitriymorkov83@gmail.com>
2025-08-16 05:17:24 +0300

    33864

--- products/models.py
+++ products/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Item(models.Model):
     item_name = models.CharField(max_length=150)
     item_price = models.DecimalField(max_digits=10, decimal_places=2)
+    item_image = models.ImageField(upload_to="items/", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
