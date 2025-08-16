97348fef1d85f56f50ba895d539e6f26bcf93b89
dimayasinskyi <dmitriymorkov83@gmail.com>
2025-08-16 05:10:13 +0300

    33865

--- account/form.py
+++ account/form.py
@@ -1,14 +1,16 @@
 from django import forms
 
-from .models import Feetback
 
-
-class Feetback(forms.ModelForm):
-    """
-    Form for leaving comments on menu items.
-
-    Has only a text field.
-    """
-    def Meta:
-        model = Feetback
-        fields = ["text"]
+class ContactForm(forms.Form):
+    name = forms.CharField(max_length=50, label="Name", widget=forms.TextInput(attrs={
+        "class": "form-control",
+        "placeholder": "Enter your full name",
+    }))
+    address = forms.CharField(required=False, label="Address", widget=forms.Textarea(attrs={
+        "class": "form-control",
+        "placeholder": "Enter your address",
+    }))
+    email = forms.EmailField(required=False, label="Email", widget=forms.EmailInput(attrs={
+        "class": "form-contol",
+        "placeholder": "Enter your email address",
+    }))
