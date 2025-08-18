eed77645223fa98eebb249c4970ac4948ad3e169
dimayasinskyi <dmitriymorkov83@gmail.com>
2025-08-18 17:13:48 +0300

    33857

--- home/views.py
+++ home/views.py
@@ -2,12 +2,20 @@ from django.shortcuts import render
 from django.conf import settings
 
 from account.form import ContactForm
+from account.models import Contact
 
 
 app_name = "home"
 
 def main_page(request):
-    return render(request, "home.html", {"form": ContactForm()})
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            Contact.objects.create(**form.cleaned_data)
+    else:
+        form = ContactForm()
+
+    return render(request, "home.html", {"form": form})
 
 def about(request):
     return render(request, "about.html")
