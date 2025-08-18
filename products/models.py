5267c11d90d6cb339d706d41f29e92488ba9bd87
dimayasinskyi <dmitriymorkov83@gmail.com>
2025-08-18 16:51:29 +0300

    33859

--- products/models.py
+++ products/models.py
@@ -9,3 +9,27 @@ class Item(models.Model):
 
     def __str__(self):
         return str(self.item_name)
+
+
+class Product(models.Model):
+    """
+    model for menu items.
+
+    Has fields:
+    - name: character field maximum 150
+    - description: the text field can be empty
+    - price: decimal number field maximum integers 10 after the point maximum 2 number
+    - created_at: automatically filled with the current date
+    """
+    name = models.CharField(max_length=150, verbose_name="Name")
+    description = models.TextField(blank=True, verbose_name="Description")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Price")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created")
+
+    class Meta:
+        ordering = ["-created_at"]
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
+    class __str__(self):
+        return f"{self.name} | {self.created_at.strftime('%d.%m.%Y')}"
