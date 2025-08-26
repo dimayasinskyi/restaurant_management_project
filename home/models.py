456dd1707d5b983ff8d7b6985041709913badcd4
dimayasinskyi <dmitriymorkov83@gmail.com>
2025-08-26 20:51:32 +0300

    39292

--- home/models.py
+++ home/models.py
@@ -6,12 +6,14 @@ class RestaurantLocationModel(models.Model):
     Model for storing information about the location of a restaurant.
 
     Has fields:
+    - phone: field characters maximum 16
     - address: text field
     - city: field characters maximum 189
     - state: field characters maximum 14 (optionally)
     - zip_code: field characters maximum 10
     - created_at: automatically filled with the current time
     """
+    phone = models.CharField(max_length=16, verbose_name="Phone")
     address = models.TextField(verbose_name="Address")
     city = models.CharField(max_length=189, verbose_name="City")
     state = models.CharField(max_length=14, blank=True, verbose_name="State")
