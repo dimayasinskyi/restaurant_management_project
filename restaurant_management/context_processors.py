c4d0c6310e3280c08384d976e5cfaa399954b45f
dimayasinskyi <dmitriymorkov83@gmail.com>
2025-08-26 20:52:39 +0300

    39292

--- restaurant_management/context_processors.py
+++ restaurant_management/context_processors.py
@@ -6,7 +6,6 @@ from home.models import RestaurantLocationModel
 def restaurant_info(request):
     return {
         "restaurant_name": getattr(settings, "RESTAURANT_NAME", "Restaurant name not provided."),
-        "restaurant_phone": getattr(settings, "RESTAURANT_CONTACT_PHONE", "Contact phone number not provided."),
         "restaurant_working_hours": getattr(settings, "WORKING_HOURS", [{"days": "Working hours not provided", "hours": ""}]),
         "restaurant_location_model": RestaurantLocationModel.objects.all(),
     }
