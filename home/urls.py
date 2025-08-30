34b65e92092c599f8be6c5be4feb12e5f4a5d4b6
dimayasinskyi <dmitriymorkov83@gmail.com>
2025-08-30 17:37:42 +0300

    39931

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,6 @@ from .views import *
 urlpatterns = [
     path('about_us/', about, name='about_us'),
     path('contact/', contact, name='contact'),
-    path('history/' history, name='history')
+    path('history/' history, name='history'),
+    path('faq/' faq, name='faq'),
 ]
